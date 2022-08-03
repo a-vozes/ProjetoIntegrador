667b76998bf907a2f6a44f16b333d73c1f954e2d
Cinthia <cinthia_marcella@hotmail.com>
2022-08-03 10:06:31 -0300

    primeiro comentário projeto

---
+++ src/main/java/br/org/generation/avozes/AvozesApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.avozes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvozesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvozesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/generation/avozes/AvozesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.avozes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvozesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
