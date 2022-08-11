85adf42ff252985f18e18b7164f634289a8ac75d
Lucas <lucascaetanocosta@gmail.com>
2022-08-11 09:50:08 -0300

    Criando o UsuarioLogin e arrumando o Model Produto e Usuario

--- src/main/java/br/org/generation/avozes/model/Usuario.java
+++ src/main/java/br/org/generation/avozes/model/Usuario.java
@@ -1,14 +1,13 @@
 package br.org.generation.avozes.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 
 @Entity
@@ -20,8 +19,10 @@ public class Usuario {
     @GeneratedValue(strategy  = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotNull(message = "O atributo Nome é Obrigatório!")
+    @Size(min = 3, max = 100)
     private String nome;
+
 
     @NotNull(message = "O usuario é obrigatório preencher este campo")
     @Email(message = "O usuario deve preencher um email valido")
@@ -32,6 +33,10 @@ public class Usuario {
     @NotBlank (message = "Insira uma senha com no minímo 8 caracteres.")
     @Size(min = 8)
     private String senha;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List<Produto> produto;
 
     public Long getId() {
         return id;
