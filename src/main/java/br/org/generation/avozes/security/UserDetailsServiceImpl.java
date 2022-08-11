3f961cf912f41e6f1684e7329a01274124e007b2
Joao Victor <jv_fn@hotmail.com>
2022-08-11 10:45:32 -0300

    update userdetailsimpl

--- src/main/java/br/org/generation/avozes/security/UserDetailsServiceImpl.java
+++ src/main/java/br/org/generation/avozes/security/UserDetailsServiceImpl.java
@@ -11,9 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.org.generation.avozes.security.UserDetailsImpl;
-
-
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
